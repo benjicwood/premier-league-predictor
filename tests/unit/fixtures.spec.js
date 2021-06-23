5826b42412c97cf8e45761d8366baaf5a43cbd6a
benjicwood <bencharlwood@gmail.com>
2021-06-23 14:12:39 +0100

    test

--- tests/unit/fixtures.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Fixtures from '@/components/Fixtures'
-
-describe('Fixtures', () => {
-    it('exists', () => {
-        const wrapper = mount(Fixtures)
-        expect(wrapper.exists()).toBe(true)
-    })
-})
